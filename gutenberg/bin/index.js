5ce47f34a38ddf79ef3a59bc43db255d2265eaf0
Abhay Vatsa <atvatsa@gmail.com>
2021-03-03 15:17:49 -0800

    save

---
+++ gutenberg/bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+const dir = require('path').resolve(__dirname, '..')
+console.log(`starting:, in directory ${dir}`)
+
+const spawn = require('child_process').spawn,
+  dev = spawn(`./node_modules/.bin/next`, ['dev', dir])
+
+dev.stdout.on('data', function (data) {
+  console.log(data.toString())
+})
+
+dev.stderr.on('data', function (data) {
+  console.log(data.toString())
+})
+
+dev.on('exit', function (code) {
+  console.log(`child process exited with code ${code.toString()}`)
+})
