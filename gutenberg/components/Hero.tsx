f7b34ef184d7d11678553a4120788781c9a08739
abhayvatsa <atvatsa@gmail.com>
2022-05-05 18:34:18 -0500

    Changing link to a button

--- gutenberg/components/Hero.tsx
+++ gutenberg/components/Hero.tsx
@@ -1,16 +1,47 @@
-import Link from 'next/link'
+import Link from 'next/link';
+
+const Button = () => (
+  <>
+    <style jsx>{`
+      .button {
+        align-self: flex-end; /* TODO: align-self should be a parameter*/
+        border-radius: var(--theme-space);
+        display: inline-block;
+        padding: var(--theme-space-4);
+        background-color: rgba(var(--highlight-accent), 1);
+      }
+
+      a {
+        color: rgba(var(--foreground-max));
+        text-decoration: none;
+        display: block;
+        text-align: right;
+      }
+    `}</style>
+    <div className="button">
+      <Link href="/a/about">
+        <a>About Me</a>
+      </Link>
+    </div>
+  </>
+);
 
 const Hero = ({ headline, text, about = false }) => {
   return (
     <>
       <style jsx>{`
         .hero {
+          display: flex;
+          flex-direction: column;
           min-width: 320px;
           padding: var(--theme-space-16) var(--theme-space-4);
+          padding-bottom: var(--theme-space-8);
           text-align: center;
+          align-items: center;
         }
 
         .headline {
+          display: block;
           color: rgba(var(--highlight-hover), 1);
           font-size: 3em;
           line-height: 1;
@@ -19,23 +50,15 @@ const Hero = ({ headline, text, about = false }) => {
         p {
           font-size: 1.5em;
           line-height: 1;
-          margin-bottom: 0px;
-        }
-        a {
-          display: block;
-          text-align: right;
         }
       `}</style>
       <section className="hero">
         <h1 className="headline">{headline}</h1>
         <p>{text}</p>
+        {about && <Button />}
       </section>
-      {about && (
-        <Link href="/a/about">
-          <a>About Me</a>
-        </Link>
-      )}
     </>
-  )
-}
-export default Hero
+  );
+};
+
+export default Hero;
